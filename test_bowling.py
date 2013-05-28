15df3889435d59b461e2d2b7a5326053720a078c
Dave Man <dmn001@gmail.com>
2013-05-28 20:34:26 +0100

    refactor rollMany

--- test_bowling.py
+++ test_bowling.py
@@ -2,6 +2,11 @@ import unittest
 from bowling import Game
  
 class BowlingTest(unittest.TestCase):
+   
+   def rollMany(self, n, pins):
+      for i in range(n):
+         self.g.roll(pins)
+
    def setUp(self):
       self.g = Game()
 
@@ -9,14 +14,14 @@ class BowlingTest(unittest.TestCase):
       self.g.roll(0)
 
    def test_gutterGame(self):
-      for i in range(20):
-         self.g.roll(0)
+      self.rollMany(20,0)
       self.assertEqual(0, self.g.score())
 
    def test_allOnes(self):
-      for i in range(20):
-         self.g.roll(1)
+      self.rollMany(20,1)
       self.assertEqual(20, self.g.score())
+
+
 
 if __name__ == "__main__":
 	unittest.main()
